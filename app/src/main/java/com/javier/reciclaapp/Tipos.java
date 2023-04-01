b523255c3bc5bcc734790a6d9999f610302c3c3b
JuanSebastianOso <plaranda13@gmail.com>
2023-04-01 12:34:38 -0500

    primer commit

---
+++ app/src/main/java/com/javier/reciclaapp/Tipos.java
@@ -0,0 +1,14 @@
+package com.javier.reciclaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tipos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipos);
+    }
+}
